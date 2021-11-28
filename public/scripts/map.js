db1457abb40690fe528d1bd49c78d1d3010a69e7
CrumpetsNTea <t.botell@gmail.com>
2021-11-27 21:32:32 -0500

    Add ability to add marker on click. Add mapInit now initializes on the user's location - they first have to approve the use of their location though via popup

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,29 +2,51 @@
 // eslint-disable-next-line func-style
 function initMap() {
 
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
+  const map = new google.maps.Map(document.getElementById("map"));
 
-  //variable to hold the options that we pass in to the .map function
-  let options = {
-    zoom: 8,
-    center: uluru
-  };
-  //new map
-  const map = new google.maps.Map(document.getElementById("map"), options);
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // Center on user's current location if geolocation prompt allowed
+    let initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map.setCenter(initialLocation);
+    map.setZoom(13);
+  }, function(positionError) {
+    // User denied geolocation prompt - default to Chicago
+    map.setCenter(new google.maps.LatLng(39.8097343, -98.5556199));
+    map.setZoom(5);
+  });
+
+
 
   // Add marker, positioned at Uluru - takes in an object with a position (lat -long) and what map we want it on
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+  // const marker = new google.maps.Marker({
+  //   position: uluru,
+  //   map: map,
+  // });
+
+
 
   let infoWindow = new google.maps.InfoWindow({
     content: '<h1>ULURU</h1>'
   });
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
+
+
+
+  map.addListener('click', function(e) {
+    placeMarker(e.latLng, map);
   });
+
+  function placeMarker(position, map) {
+    let marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
+    map.panTo(position);
+
+    marker.addListener('dblclick', function() {
+      infoWindow.open(map, marker);
+    });
+  }
+
 }
 
 
