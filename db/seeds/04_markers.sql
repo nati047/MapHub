8499f66305e927e64e504eb4a36f432cfa470a5f
CrumpetsNTea <t.botell@gmail.com>
2021-11-29 16:14:48 -0500

    add markers schema and seeds

---
+++ db/seeds/04_markers.sql
@@ -0,0 +1,13 @@
+INSERT INTO markers (map_id, markername, description, latitude, longitude)
+VALUES (1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.755128, -83.454298),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.73, -82.1),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.2, -82.1),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 35.9, -83.2),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.555128, -83.6),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.7, -83.454298),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.6, -83.454298),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.5, -83.454298),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.5, -83.4),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.5, -83.3),
+(1, 'Sasquatch Sighting', 'Saw Sasquatch', 36.5, -83.2);
+
