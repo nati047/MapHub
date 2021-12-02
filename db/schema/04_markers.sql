7bb58bb74410fc5267889a760da04eb420da6e50
CrumpetsNTea <t.botell@gmail.com>
2021-12-02 12:13:45 -0500

    removed NOT NULL clause from markers CREATE TABLE as was causing error when insterting marker on addMarker function because when you place a new marker you cannot declare it's title - however when the user edits the marker they can add a title

--- db/schema/04_markers.sql
+++ db/schema/04_markers.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS markers CASCADE;
 CREATE TABLE markers (
   id SERIAL PRIMARY KEY NOT NULL,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
-  markername VARCHAR(255) NOT NULL,
+  markername VARCHAR(255),
   description TEXT DEFAULT 'Enter a description',
   picture_url VARCHAR(255) DEFAULT 'Enter a url',
   latitude VARCHAR(255) NOT NULL,
